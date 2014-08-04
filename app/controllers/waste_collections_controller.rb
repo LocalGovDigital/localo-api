5354f5947bb5b6417b71326a3d7f0a12f9234c58
Dan Blundell <danblundell@me.com>
2014-08-04 17:55:34 +0100

    Move wastecollection to its own model

--- app/controllers/waste_collections_controller.rb
+++ app/controllers/waste_collections_controller.rb
@@ -1,30 +1,23 @@
 require 'rest_client'
 
-# TODO: move to model?
-class Collection
-  attr :day, true
-  attr :last_collection, true
-  attr :end_date, true
-  attr :frequency, true
-end
-
 class WasteCollectionsController < ApplicationController
 
   URL = 'https://www2.guildford.gov.uk/Services/CustomerPoint/Cleansing/Property/100061403569/ActiveCollections'
 
   api :GET, '/waste_collections'
   respond_to :json
+
   def index
     response = RestClient.get URL
     data = Nokogiri::XML response
 
     # puts data.css("Collection").map {|node| node.children.text}
 
-    c = Collection.new
+    c = WasteCollection.new
     c.day = 'Monday'
     @waste_collections = []
     data.css("Collection").each do |collection_in|
-      collection = Collection.new
+      collection = WasteCollection.new
       collection.day = collection_in.css('Day').text
       collection.last_collection = collection_in.css('LastCollection').text
       collection.end_date = collection_in.css('EndDate').text
---
+++ app/models/waste_collection.rb
@@ -0,0 +1,6 @@
+class WasteCollection
+  attr :day, true
+  attr :last_collection, true
+  attr :end_date, true
+  attr :frequency, true
+end
