091e169f3b6ff56c94512a9fe305e24583b3142c
Dan Blundell <danblundell@me.com>
2014-08-04 17:56:16 +0100

    rename apipie endpoint to docs

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,7 +1,7 @@
 Apipie.configure do |config|
   config.app_name                = "RailsBootstrap"
   config.api_base_url            = "/api"
-  config.doc_base_url            = "/apipie"
+  config.doc_base_url            = "/docs"
   # where is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/*.rb"
 end
